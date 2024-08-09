9bc2a056e189392dab5759bffb302fe1aab18aa7
Regige <144334703+Regige@users.noreply.github.com>
2024-08-09 16:01:43 +0200

    Added custom user

---
+++ accounts/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from .models import CustomUser
+from .forms import CustomUserCreationForm
+from django.contrib.auth.admin import UserAdmin
+
+# Register your models here.
+
+# admin.site.register(CustomUser)
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    add_form = CustomUserCreationForm
+    fieldsets = (
+        *UserAdmin.fieldsets,
+        (
+            'Individual Data',
+            {
+                'fields': (
+                    'phone_number',
+                    'is_verified'
+                )
+            }
+        )
+    )
--- videoflix/settings.py
+++ videoflix/settings.py
@@ -38,8 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'accounts',
     'video'
 ]
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -123,3 +126,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
