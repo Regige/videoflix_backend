0d49adff17b08fd302d5b25785d31ebee321a3a5
Regige <144334703+Regige@users.noreply.github.com>
2024-08-23 17:34:00 +0200

    Added comments

--- video/signals.py
+++ video/signals.py
@@ -1,5 +1,5 @@
 from .models import Video
-from .tasks import convert720p, convert360p, convert1080p
+from .tasks import convert720p, convert480p, convert1080p
 from django.dispatch import receiver
 from django.db.models.signals import post_save, post_delete
 import os
@@ -10,9 +10,9 @@ def video_post_save(sender, instance, created, **kwargs):
     if created:
         queue = django_rq.get_queue('default', autocommit=True)
         queue.enqueue(convert720p, instance.video_file.path)
-        queue.enqueue(convert360p, instance.video_file.path)
+        queue.enqueue(convert480p, instance.video_file.path)
         queue.enqueue(convert1080p, instance.video_file.path)
-        # convert360p(instance.video_file.path)
+        # convert480p(instance.video_file.path)
         # convert720p(instance.video_file.path)
         # convert1080p(instance.video_file.path)
 
@@ -25,9 +25,16 @@ def video_post_save(sender, instance, created, **kwargs):
 @receiver(post_delete, sender=Video)
 def auto_delete_file_on_delete(sender, instance, **kwargs):
     """
-    Deletes file from filesystem
-    when corresponding `MediaFile` object is deleted.
+    Deletes the main file and all related files from the filesystem
+    when the corresponding `Video` object is deleted.
     """
     if instance.video_file:
-        if os.path.isfile(instance.video_file.path):
-            os.remove(instance.video_file.path)
+        main_file_path = instance.video_file.path
+        if os.path.isfile(main_file_path):
+            os.remove(main_file_path)
+        
+        base_path = main_file_path.rsplit('.', 1)[0]
+        for suffix in ['_480p.mp4', '_720p.mp4', '_1080p.mp4']:
+            related_file_path = f"{base_path}{suffix}"
+            if os.path.isfile(related_file_path):
+                os.remove(related_file_path)
