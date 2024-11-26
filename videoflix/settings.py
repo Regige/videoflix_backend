3495951d5040198a82dd0ac6f0ea6eb02f159bd3
Regina Gering <Regina.Gering@hotmail.de>
2024-11-26 13:23:57 +0000

    Changed credentials postgres

--- videoflix/settings.py
+++ videoflix/settings.py
@@ -126,11 +126,11 @@ WSGI_APPLICATION = 'videoflix.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'videoflix',
-        'USER': 'regina',
-        'PASSWORD': 'SaureSahne123',
-        'HOST': 'localhost',
-        'PORT': '',
+        'NAME': os.environ.get('DATABASE_NAME'),
+        'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST': os.environ.get('DATABASE_HOST'),
+        'PORT': os.environ.get('DATABASE_PORT'),
     }
 }
 
